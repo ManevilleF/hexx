74df984ec031fdec2ce4bc5d3274a93caed1498e
Félix Lescaudey de Maneville <felix.maneville@gmail.com>
2023-05-02 15:21:57 +0200

    Builder example

--- examples/mesh_builder.rs
+++ examples/mesh_builder.rs
@@ -3,33 +3,52 @@ use bevy::{
     prelude::*,
     render::{mesh::Indices, render_resource::PrimitiveTopology},
 };
+use bevy_inspector_egui::{prelude::*, quick::ResourceInspectorPlugin};
 use hexx::*;
+
+#[derive(Debug, Resource)]
+struct HexInfo {
+    pub layout: HexLayout,
+    pub mesh_entity: Entity,
+    pub mesh_handle: Handle<Mesh>,
+}
+
+#[derive(Debug, Resource, Reflect, InspectorOptions)]
+#[reflect(Resource, InspectorOptions)]
+struct BuilderParams {
+    #[inspector(min = 0.0, max = 50.0)]
+    pub height: f32,
+    #[inspector(min = 0, max = 50)]
+    pub subdivisions: usize,
+    pub top_face: bool,
+    pub bottom_face: bool,
+}
 
 pub fn main() {
     App::new()
+        .register_type::<BuilderParams>()
+        .init_resource::<BuilderParams>()
         .insert_resource(AmbientLight {
             brightness: 0.1,
             ..default()
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(WireframePlugin)
+        .add_plugin(ResourceInspectorPlugin::<BuilderParams>::default())
         .add_startup_system(setup)
         .add_system(animate)
+        .add_system(update_mesh)
         .run();
-}
-
-#[derive(Debug, Resource)]
-struct HexInfo {
-    pub mesh_entity: Entity,
 }
 
 /// 3D Orthogrpahic camera setup
 fn setup(
     mut commands: Commands,
+    params: Res<BuilderParams>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let transform = Transform::from_xyz(0.0, 0.0, 50.0).looking_at(Vec3::ZERO, Vec3::Y);
+    let transform = Transform::from_xyz(0.0, 0.0, 30.0).looking_at(Vec3::ZERO, Vec3::Y);
     commands.spawn(Camera3dBundle {
         transform,
         ..default()
@@ -39,22 +58,26 @@ fn setup(
         ..default()
     });
     let layout = HexLayout::default();
-    let mesh = ColumnMeshBuilder::new(&layout, 15.0)
-        .with_subdivisions(5)
+    let mesh = ColumnMeshBuilder::new(&layout, params.height)
+        .with_subdivisions(params.subdivisions)
         .build();
-    let mesh = meshes.add(compute_mesh(mesh));
-    let material = materials.add(Color::WHITE.into());
+    let mesh_handle = meshes.add(compute_mesh(mesh));
+    let material = materials.add(Color::CYAN.into());
     let mesh_entity = commands
         .spawn((
             PbrBundle {
-                mesh,
+                mesh: mesh_handle.clone(),
                 material,
                 ..default()
             },
             Wireframe,
         ))
         .id();
-    commands.insert_resource(HexInfo { mesh_entity });
+    commands.insert_resource(HexInfo {
+        layout,
+        mesh_entity,
+        mesh_handle,
+    });
 }
 
 fn animate(info: Res<HexInfo>, mut transforms: Query<&mut Transform>, time: Res<Time>) {
@@ -66,6 +89,23 @@ fn animate(info: Res<HexInfo>, mut transforms: Query<&mut Transform>, time: Res<
     transform.rotate_z(delta_time);
 }
 
+fn update_mesh(params: Res<BuilderParams>, info: Res<HexInfo>, mut meshes: ResMut<Assets<Mesh>>) {
+    if !params.is_changed() {
+        return;
+    }
+    let mut new_mesh =
+        ColumnMeshBuilder::new(&info.layout, params.height).with_subdivisions(params.subdivisions);
+    if !params.top_face {
+        new_mesh = new_mesh.without_top_face();
+    }
+    if !params.bottom_face {
+        new_mesh = new_mesh.without_bottom_face();
+    }
+    let new_mesh = compute_mesh(new_mesh.build());
+    let mesh = meshes.get_mut(&info.mesh_handle).unwrap();
+    *mesh = new_mesh;
+}
+
 /// Compute a bevy mesh from the layout
 fn compute_mesh(mesh_info: MeshInfo) -> Mesh {
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
@@ -74,4 +114,15 @@ fn compute_mesh(mesh_info: MeshInfo) -> Mesh {
     mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, mesh_info.uvs.to_vec());
     mesh.set_indices(Some(Indices::U16(mesh_info.indices)));
     mesh
+}
+
+impl Default for BuilderParams {
+    fn default() -> Self {
+        Self {
+            height: 10.0,
+            subdivisions: 3,
+            top_face: true,
+            bottom_face: true,
+        }
+    }
 }
