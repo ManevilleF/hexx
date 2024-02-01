ef6db2da8867b4be4974f2b9fd24a91b677efca9
Félix Lescaudey de Maneville <felix.maneville@gmail.com>
2024-02-01 10:43:56 +0100

    MSRV 1.65.0

--- src/hex/convert.rs
+++ src/hex/convert.rs
@@ -17,8 +17,10 @@ impl From<[i32; 2]> for Hex {
 
 impl From<(f32, f32)> for Hex {
     #[inline]
-    fn from(v: (f32, f32)) -> Self {
-        Self::round(v.into())
+    // We allow this lint to avoid bumping MSRV
+    #[allow(clippy::tuple_array_conversions)]
+    fn from((x, y): (f32, f32)) -> Self {
+        Self::round([x, y])
     }
 }
 
