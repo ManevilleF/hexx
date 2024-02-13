91e03d509331aab3ee54be5fa3545de2a04f84e5
Félix Lescaudey de Maneville <felix.maneville@gmail.com>
2024-02-13 10:15:42 +0100

    inlining

--- src/hex/ord.rs
+++ src/hex/ord.rs
@@ -14,54 +14,63 @@ pub struct OrdByXY(pub Hex);
 pub struct OrdByYX(pub Hex);
 
 impl From<Hex> for OrdByLength {
+    #[inline]
     fn from(value: Hex) -> Self {
         Self(value)
     }
 }
 
 impl From<Hex> for OrdByXY {
+    #[inline]
     fn from(value: Hex) -> Self {
         Self(value)
     }
 }
 
 impl From<Hex> for OrdByYX {
+    #[inline]
     fn from(value: Hex) -> Self {
         Self(value)
     }
 }
 
 impl PartialOrd for OrdByLength {
+    #[inline]
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
 impl PartialOrd for OrdByXY {
+    #[inline]
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
 impl PartialOrd for OrdByYX {
+    #[inline]
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
 impl Ord for OrdByLength {
+    #[inline]
     fn cmp(&self, other: &Self) -> Ordering {
         self.0.length().cmp(&other.0.length())
     }
 }
 
 impl Ord for OrdByXY {
+    #[inline]
     fn cmp(&self, other: &Self) -> Ordering {
         self.0.x.cmp(&other.0.x).then(self.0.y.cmp(&other.0.y))
     }
 }
 
 impl Ord for OrdByYX {
+    #[inline]
     fn cmp(&self, other: &Self) -> Ordering {
         self.0.y.cmp(&other.0.y).then(self.0.x.cmp(&other.0.x))
     }
