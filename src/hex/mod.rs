8cbaf0d0b5d12346f03d6471a81eea69237c9d48
Félix de Maneville <felix.maneville@gmail.com>
2025-08-18 09:51:05 +0200

    chore: More euclidean norm methods

--- src/hex/mod.rs
+++ src/hex/mod.rs
@@ -1,6 +1,11 @@
 #![allow(clippy::inline_always)]
 /// Type conversions
 mod convert;
+/// Euclidean norm from Xiangguo Li's 2013 [Paper]. ([DOI])
+///
+/// [Paper]: https://scholar.google.com/scholar?q=Storage+and+addressing+scheme+for+practical+hexagonal+image+processing
+/// [DOI]: https://doi.org/10.1117/1.JEI.22.1.010502
+mod euclidean;
 /// Hexagonal grid utilities, like edge and vertices
 #[cfg(feature = "grid")]
 pub mod grid;
@@ -619,29 +624,6 @@ impl Hex {
         self.const_sub(rhs).ulength()
     }
 
-    /// Computes euclidean distance from `self` to `rhs` as a floating point
-    /// number in the Cartesian coordinate system.
-    /// Euclidean distance can vary for coordinates in the same range, and
-    /// can be used for operations outside of the hexagonal space, like
-    /// checking if coordinates are in a circular range instead of an
-    /// hexagonal range
-    ///
-    /// Note: For most cases you should use an [`crate::HexLayout`]
-    ///
-    /// > Source:
-    /// > Xiangguo Li's 2013 [Paper]. ([DOI]) gives a formula for Euclidean
-    /// > distance
-    ///
-    /// [Paper]: https://scholar.google.com/scholar?q=Storage+and+addressing+scheme+for+practical+hexagonal+image+processing
-    /// [DOI]: https://doi.org/10.1117/1.JEI.22.1.010502
-    #[must_use]
-    #[expect(clippy::cast_precision_loss)]
-    pub fn euclidean_distance_to(self, rhs: Self) -> f32 {
-        let dx = (rhs.x - self.x) as f32;
-        let dy = (rhs.y - self.y) as f32;
-        dx.mul_add(dy, dx.mul_add(dx, dy * dy)).sqrt()
-    }
-
     #[inline]
     #[must_use]
     /// Retrieves the hexagonal neighbor coordinates matching the given
@@ -1016,31 +998,6 @@ impl Hex {
             }),
             count: Self::range_count(range) as usize,
         }
-    }
-
-    /// Retrieves all [`Hex`] around `self` in a given circular `range`.
-    ///
-    /// > See also [`Hex::range`] for hexagonal ranges
-    ///
-    /// Note that this implementation is very naive and inefficient.
-    /// Use sparingly
-    ///
-    /// # Example
-    ///
-    /// ```rust
-    /// # use hexx::*;
-    /// let coord = hex(12, 34);
-    /// assert_eq!(coord.circular_range(0.0).count(), 1);
-    /// assert_eq!(coord.circular_range(1.0).count(), 7);
-    /// ```
-    #[expect(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
-    pub fn circular_range(self, range: f32) -> impl Iterator<Item = Self> {
-        let radius = range.ceil() as u32;
-        // TODO: Improve this computation to have the smallest hexagon
-        // which fits the circle
-        let hex_range = radius + (range / 6.0).floor() as u32;
-        self.range(hex_range)
-            .filter(move |h| self.euclidean_distance_to(*h) <= range)
     }
 
     #[doc(alias = "excluding_range")]
